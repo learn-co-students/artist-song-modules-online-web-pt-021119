d6d973b04240dd57daa793c17c9ac11b1c79bffe
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-04-03 08:11:35 +0000

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,21 @@
+module Memorable
+
+  module ClassMethods
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.count
+  end
+  end
+
+end
+
+class Artist
+  extend Memorable
+end
+
+class Song
+  extend Memorable
+end
